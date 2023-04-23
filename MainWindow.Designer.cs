fe61d242f4560590f2cac60d51099231f03b0aed
the-woose <yavuzsava@yahoo.com.tr>
2023-04-24 01:14:30 +0300

    main menu update

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,18 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            btnExit = new Button();
+            btnMin = new Button();
+            btnHucre = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Menu;
+            pictureBox1.InitialImage = Properties.Resources.Menu;
+            pictureBox1.Location = new Point(35, 44);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(333, 355);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = Color.Red;
+            btnExit.FlatAppearance.BorderSize = 0;
+            btnExit.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnExit.ForeColor = Color.White;
+            btnExit.Location = new Point(736, 14);
+            btnExit.Margin = new Padding(5);
+            btnExit.MaximumSize = new Size(50, 50);
+            btnExit.MinimumSize = new Size(50, 50);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(50, 50);
+            btnExit.TabIndex = 1;
+            btnExit.Text = "X";
+            btnExit.UseVisualStyleBackColor = false;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnMin
+            // 
+            btnMin.BackColor = Color.LightBlue;
+            btnMin.FlatAppearance.BorderSize = 0;
+            btnMin.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnMin.ForeColor = Color.Black;
+            btnMin.Location = new Point(676, 14);
+            btnMin.Margin = new Padding(5);
+            btnMin.MaximumSize = new Size(50, 50);
+            btnMin.MinimumSize = new Size(50, 50);
+            btnMin.Name = "btnMin";
+            btnMin.Size = new Size(50, 50);
+            btnMin.TabIndex = 2;
+            btnMin.Text = "__";
+            btnMin.UseVisualStyleBackColor = false;
+            btnMin.Click += btnMin_Click;
+            // 
+            // btnHucre
+            // 
+            btnHucre.Location = new Point(524, 110);
+            btnHucre.Name = "btnHucre";
+            btnHucre.Size = new Size(119, 41);
+            btnHucre.TabIndex = 3;
+            btnHucre.Text = "btnHücre";
+            btnHucre.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnHucre);
+            Controls.Add(btnMin);
+            Controls.Add(btnExit);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "MainWindow";
             Text = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnExit;
+        private Button btnMin;
+        private Button btnHucre;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -6,5 +6,15 @@ namespace BTE_202___2023_Grup_C_Uygulaması
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
     }
 }
