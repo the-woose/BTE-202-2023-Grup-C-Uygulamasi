88380b1a1e96d9f9e05672c047c3a729cc22317f
the-woose <yavuzsava@yahoo.com.tr>
2023-04-23 22:31:41 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace BTE_202___2023_Grup_C_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace BTE_202___2023_Grup_C_Uygulaması
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
